541bffde59e046ece652832654a8a95472e5b86b
dnicifor <dnicifor@users.noreply.github.com>
2020-10-30 16:46:17 -0400

    adding to certification js adding entry fields to html

---
+++ app/public/js/certifications.app.js
@@ -0,0 +1,60 @@
+var app = new Vue({
+  el: '#certs',
+  data: {
+    certifications: [
+      
+
+
+    ],
+
+    newCert: {
+      CertificationID: '',
+    	CertificationName: '',
+    	CertifyingAgency: '',
+    	ExpirationPeriod: '',
+    }
+  },
+  methods: {
+    fetchUser() {
+      fetch('api/data_entry/')
+      .then(response => response.json())
+      .then(json => {
+        this.certifications=json;
+        console.log(this.certifications);
+      });
+    },
+      createCert(){
+        fetch('api/data_entry/addCert.php', {
+          method: 'POST',
+          body: JSON.stringify(this.newCert),
+          headers: {
+            "Content-Type":"application/json; charset=utf-8"
+
+          }
+        })
+        .then(response => response.json())
+        .then( json => {
+          console.log("Returned from post:", json);
+          this.certifications = json;
+          this.newCert = this.newIdData();
+        });
+        console.log("Creating (POSTing)...!");
+        console.log(this.newCert);
+
+      },
+      newIdData() {
+        return {
+          CertificationID: '',
+        	CertificationName: '',
+        	CertifyingAgency: '',
+        	ExpirationPeriod: '',
+        }
+      }
+    },
+
+    created(){
+      this.fetchUser();
+
+
+    }
+  });
