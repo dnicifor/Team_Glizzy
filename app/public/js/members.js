4a2636869b6cc81607790f3889b258a39366f590
Mackenzie Lim <macklim8@gmail.com>
2020-10-30 13:50:14 -0400

    Certifications HTMl & JS
    certReport.php still needs help

--- app/public/js/members.js
+++ app/public/js/members.js
@@ -14,53 +14,19 @@ memberApp = new Vue({
       ActiveStatus: '',
     }],
 
-  created() {
-    this.fetchMember();
-  },
+    created() {
+      this.fetchMember();
+    },
 
-  methods: {
-    fetchMember() {
-      fetch('api/Reports/')
-        .then(response => response.json())
-        .then(json => {
-          this.mem=json;
-          console.log(this.mem);
-        });
+    methods: {
+      fetchMember() {
+        fetch('api/Reports/memReport.php')
+          .then(response => response.json())
+          .then(json => {
+            this.mem=json;
+            console.log(this.mem);
+          });
+        },
       },
     },
-  },
-
-/*  mounted: function() {
-          axios.get('https://jsonplaceholder.typicode.com/users')
-              .then(response => {
-                this.members = response.data;
-                console.log(response);
-              })
-              .catch(error => {
-                console.log(error);
-              });*/
-  /*
-  created(){
-    this.fetchInfo();
-  },
-
-  methods: {
-    fetchInfo: function(){
-      fetch('https://randomuser.me/api/')
-      .then(response => response.json())
-      .then(data => {
-        var memberInfo = data.results[0];
-        console.log(memberInfo);
-        this.userFirst = userInfo.first;
-        this.userLast = userInfo.last;
-        this.userDob = userInfo.dob;
-        this.userGender = userInfo.gender;
-        this.userStart = userInfo.start;
-        this.userPosition = userInfo.Position;
-        this.userActive = userInfo.active;
-        this.userRadio = userInfo.radio;
-      });
-    },
-  },*/
-
   })
