c51fa1531784d11086d2326bf9aee79c042516d1
emsanner <emsanner@iu.edu>
2020-11-01 16:09:06 -0500

    changes to update

--- app/public/api/data_entry/certifications/update.php
+++ app/public/api/data_entry/certifications/update.php
@@ -12,7 +12,7 @@ $db = DbConnection::getConnection();
 // Step 2: Create & run the query
 // Note the use of parameterized statements to avoid injection
 $stmt = $db->prepare(
-  "UPDATE Certification SET
+  "UPDATE Certifications SET
     CertificateName=?,
     ExpirationID=?
     WHERE CertificationID=?"
